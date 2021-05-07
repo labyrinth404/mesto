4c3e0bd8f9bbe6ba6ed900068f86f12b43a01692
Bogdan G. Kovriga <bkovriga@list.ru>
2021-05-07 20:32:08 +0700

    feat: Сделано меню добавления мест

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,26 @@
-let formElement = document.querySelector('.popup__container')
-let nameInput = formElement.querySelector('[name="popup-name-form"]');
-let jobInput = formElement.querySelector('[name="popup-description-form"]');
+let popupElement = document.querySelector('.popup__container')
+let nameInput = popupElement.querySelector('[name="popup-name-form"]');
+let jobInput = popupElement.querySelector('[name="popup-description-form"]');
 
 let popup = document.querySelector('.popup'),
     popupButtonClose = popup.querySelector('.popup__button-close'),
     popupButtonSave = popup.querySelector('.popup__button-save');
 
-let profileInfo = document.querySelector('.profile__info'),
+let form = document.querySelector('.form'),
+    formButtonCLose = form.querySelector('.form__button-close'),
+    formButtonSave = form.querySelector('.form__button-save'),
+    formElement = form.querySelector('.form__container'),
+    nameMesto = formElement.querySelector('[name="mesto-name-form"]'),
+    urlMesto = formElement.querySelector('[name="mest-url-form]');
+
+let profileInfo = document.querySelector('.profile'),
     profileName = profileInfo.querySelector('.profile__name'),
     profileDescription = profileInfo.querySelector('.profile__description'),
-    profileButtonEdit = profileInfo.querySelector('.profile__button-edit');
+    profileButtonEdit = profileInfo.querySelector('.profile__button-edit'),
+    profileButtonAdd = profileInfo.querySelector('.profile__button-add');
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
     closePopup();
@@ -27,12 +34,28 @@ function openPopup() {
     popup.classList.add('popup_opened');
 
 }
+
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
+function openForm() {
+    form.classList.add('form_opened');
+}
+
+function closeForm() {
+    form.classList.remove('form_opened');
+    nameMesto.value = '';
+    urlMesto.value = '';
+
+}
 
 
-formElement.addEventListener('submit', formSubmitHandler);
+
+popupElement.addEventListener('submit', formSubmitHandler);
+
 popupButtonClose.addEventListener('click', closePopup);
 profileButtonEdit.addEventListener('click', openPopup);
+
+formButtonCLose.addEventListener('click', closeForm);
+profileButtonAdd.addEventListener('click', openForm);
