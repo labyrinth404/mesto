53d859ee2b574ac42bd3a064fbe66583400e4fd5
Bogdan <bkovriga@list.ru>
2021-07-21 15:02:50 +0700

    fix babel and postcss config

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|ico|otf)$/,
         type: 'asset/resource',
       },
       {
